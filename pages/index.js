a7c1ad3f5ea24a1bbc512bb066fd7119a155c34b
AhrensOG <ahrensgabriel09@gmail.com>
2022-11-12 23:14:13 -0300

    Initial commit with TailwindCSS

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+
+  return (
+    <div>
+        <h1 className='text-6xl text-center p-2'>
+          Welcome to <a className='hover:underline text-[#0070f3]' href="https://nextjs.org">Next.js!</a>
+        </h1>
+    </div>
+  )
+}
