17fd00e3096d675c7213cea7a8aa277e185abc88
AhrensOG <ahrensgabriel09@gmail.com>
2022-11-19 16:27:55 -0300

    Add NavBar component in home

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-import Head from "next/head"
-import Tag from "../components/Tag"
-import TopNewSection from "../components/TopNewSection/TopNewSection"
+import Head from "next/head";
+import TopNewSection from "../components/TopNewSection/TopNewSection";
+import NavBar from "../components/NavBar/NavBar";
 
 export default function Home() {
-
   return (
     <div>
       <Head>
         <title>Newspaper | Home</title>
       </Head>
-      <TopNewSection/>
-      <h1 className='text-6xl text-center p-2'>
-      Welcome to <a className='hover:underline text-[#0070f3]' href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <NavBar />
+      <TopNewSection />
     </div>
-  )
+  );
 }
