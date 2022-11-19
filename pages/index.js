2f9ee2be81d0fea79196306088155b1ac3e2b44d
AhrensOG <ahrensgabriel09@gmail.com>
2022-11-19 14:28:19 -0300

    Add TopNewSection in home component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
+import Head from "next/head"
+import Tag from "../components/Tag"
+import TopNewSection from "../components/TopNewSection/TopNewSection"
+
 export default function Home() {
 
   return (
     <div>
-        <h1 className='text-6xl text-center p-2'>
-          Welcome to <a className='hover:underline text-[#0070f3]' href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Head>
+        <title>Newspaper | Home</title>
+      </Head>
+      <TopNewSection/>
+      <h1 className='text-6xl text-center p-2'>
+      Welcome to <a className='hover:underline text-[#0070f3]' href="https://nextjs.org">Next.js!</a>
+      </h1>
     </div>
   )
 }
