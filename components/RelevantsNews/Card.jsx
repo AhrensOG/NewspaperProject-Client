ad76c559a980d80d0dc7ca17f8c7619d5e26e8f4
AhrensOG <ahrensgabriel09@gmail.com>
2022-11-20 16:15:01 -0300

    Add link to detail post

--- components/RelevantsNews/Card.jsx
+++ components/RelevantsNews/Card.jsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Card ({image, title}) {
   return (
     <div className="">
       <div className="pb-2 ">
-        <img className="w-full md:h-[150px] lg:h-[250px] cursor-pointer"  src={image} alt="Image from important card" />
+        <Link href={`/detalle/${title}`}>
+          <img className="w-full md:h-[150px] lg:h-[250px] cursor-pointer"  src={image} alt="Image from important card" />
+        </Link>  
       </div>
       <div className="pt-2">
-        <h2 className="font-custom font-semibold md:text-[1.2rem] lg:text-[1.5rem] cursor-pointer hover:underline underline-offset-1" >{title}</h2>
+        <Link href={`/detalle/${title}`}>
+          <h2 className="font-custom font-semibold md:text-[1.2rem] lg:text-[1.5rem] cursor-pointer hover:underline underline-offset-1" >{title}</h2>
+        </Link>  
       </div> 
     </div>
   )
