d323b0398be8839caf492192189bc6ce502dab0f
AhrensOG <ahrensgabriel09@gmail.com>
2022-11-19 21:12:14 -0300

    Fixed comflicts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import TopNewSection from "../components/TopNewSection/TopNewSection";
 import NavBar from "../components/NavBar/NavBar"
 import TrendingSection from "../components/TrendingSection/TrendingSection"
+import ImportantNews from "../components/RelevantsNews/Relevants"
 
 
 
