1a7289c0af855faacd9b2e37e412ae5c17abc3a9
AhrensSG <guillermoahrens@gmail.com>
2022-11-28 22:01:01 -0300

    weather-component-added

--- components/MiddleComponent/Card.jsx
+++ components/MiddleComponent/Card.jsx
@@ -1,26 +1,49 @@
-export default function Card ({buy, sell, variation, name}) {
+export default function Card ({buy1, buy2, sell1, sell2, variation1, variation2, name1, name2}) {
 
   return(
-    <div className="bg-slate-700 rounded-md">
-      <div>
-        <h2 className=" font-roboto flex justify-center text-sky-400">
-          {name}
-        </h2>
-      </div>
+    <div className="bg-[#fff] rounded-md border border-sky-500">
       <div className="flex">
         <div className="basis-1/2">
-          <p className=" flex justify-center text-white font-roboto">COMPRA</p>
-          <h3 className=" flex justify-center text-white">{buy}</h3>
+          <h2 className=" font-roboto flex justify-center text-[#959595]">
+            {name1}
+          </h2>
         </div>
         <div className="basis-1/2">
-          <p className="  flex justify-center text-white font-roboto">VENTA</p>
-          <h3 className="  flex justify-center text-white">{sell}</h3>
+          <h2 className=" font-roboto flex justify-center text-[#959595]">
+            {name2}
+          </h2>
         </div>
       </div>
-      <div className=" ">
-        <p className="  flex justify-center text-white font-roboto ">VARIACIÓN</p>
-        <h2 className="  flex justify-center text-white ">{variation}</h2>
-      </div>
+      <div>
+        <div className="flex">
+          <div className="basis-1/2">
+            <p className=" flex justify-center text-[#959595] font-roboto mx-3">COMPRA</p>
+            <h3 className=" flex justify-center text-[#18937e]">{buy1}</h3>
+          </div>
+          <div className="basis-1/2">
+            <p className="  flex justify-center text-[#959595] font-roboto mx-3">VENTA</p>
+            <h3 className="  flex justify-center text-[#18937e]">{sell1}</h3>
+          </div>
+          <div className="basis-1/2">
+            <p className=" flex justify-center text-[#959595] font-roboto mx-3">COMPRA</p>
+            <h3 className=" flex justify-center text-[#18937e]">{buy2}</h3>
+          </div>
+          <div className="basis-1/2">
+            <p className="  flex justify-center text-[#959595] font-roboto mx-3">VENTA</p>
+            <h3 className="  flex justify-center text-[#18937e]">{sell2}</h3>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="basis-1/2">
+          <p className="flex justify-center text-[#959595] font-roboto ">VARIACIÓN</p>
+          <h2 className="flex justify-center text-[#18937e] ">{variation1}</h2>
+          </div>
+          <div className="basis-1/2">
+          <p className="flex justify-center text-[#959595] font-roboto ">VARIACIÓN</p>
+          <h2 className="flex justify-center text-[#18937e] ">{variation2}</h2>
+          </div>
+        </div>
+        </div>
     </div>
   )
 
--- components/MiddleComponent/index.jsx
+++ components/MiddleComponent/index.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Card from "./Card";
+import Dolar from "./Dolar";
+import IconLogo from "./IconLogo";
+import Icons from "./Icons";
+import Weather from "./Weather";
 
 export default function MiddleComponent() {
   const [dolar, setDolar] = useState([]);
@@ -16,38 +19,13 @@ export default function MiddleComponent() {
   }, []);
 
   return (
-    <div className="flex">
-      <div className="basis-1/4 pt-[30px] ">
-        <div className="flex">
-          <div className="ml-[20px] mr-[20px] w-[50px] h-[50px]">
-            <img src="/instagramLogo.png" alt="" />
-          </div>
-          <div className="ml-[20px] mr-[20px] w-[50px] h-[50px]">
-            <img src="/twitterLogo.png" alt="" />
-          </div>
-          <div className=" ml-[20px] mr-[20px] w-[55px] h-[60px]">
-            <img src="/youtubeLogo.png" alt="" />
-          </div>
-          <div className=" ml-[20px] mr-[20px] w-[50px] h-[50px]">
-            <img src="/facebookLogo.png" alt="" />
-          </div>
-        </div>
+    <div>
+      <div className="flex">
+      <Weather/>
+        <IconLogo/>
+        <Dolar dolar={dolar}/>
       </div>
-      <div className=" basis-1/2 flex justify-center">
-        <div className="  w-[240px] h-[130px]">
-          <img src="/Logo.png" alt="Logo png" className="w-full h-full" />
-        </div>
-      </div>
-      <div className="basis-1/4 pr-[10px]" >
-        <div className="flex">
-          <div className="basis-1/2 mr-[10px] "> 
-            <Card sell={dolar[0]?.casa.venta} buy={dolar[0]?.casa.compra} variation={dolar[0]?.casa.variacion} name={dolar[0]?.casa.nombre} />
-          </div>
-          <div className="basis-1/2 ml-[10px]">
-          <Card sell={dolar[1]?.casa.venta} buy={dolar[1]?.casa.compra} variation={dolar[1]?.casa.variacion} name={dolar[1]?.casa.nombre} />
-          </div>
-        </div>
-      </div>
+      <Icons/>
     </div>
   );
 }
